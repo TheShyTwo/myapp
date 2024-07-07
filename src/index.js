dc4a6836df8f44f8c6873a2112f5b9b10d5976d2
TheShyy2 <longbtceth100199@gmail.com>
2024-07-07 17:25:18 +0700

    using husk

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const route = require('./routes/index');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+        app.use(express.json());
 
 // app.use(morgan('combined'))
 
-app.engine('hbs', engine({ extname: '.hbs' }));
+        app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
